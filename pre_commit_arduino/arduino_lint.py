5f96a6a4d00d86f92986f2a5ea242282382ab9f7
nulltek <steve.public@nulltek.xyz>
2022-02-07 13:31:58 +1300

    Resolving the repo dir for arduino_lint argument. Updating to use pre-commit-pycli 0.2.4. Allowing arduino-lint to fail on warning if required.

--- pre_commit_arduino/arduino_lint.py
+++ pre_commit_arduino/arduino_lint.py
@@ -5,26 +5,40 @@ Script template based on https://github.com/pocc/pre-commit-hooks/
 """
 import sys
 from typing import List
+from argparse import ArgumentParser
 
 from pre_commit_pycli.cli import StaticAnalyzerCmd
+from pathlib import Path
 
 
 class ArduinoLint(StaticAnalyzerCmd):
     """Class for calling the Arduino Lint CLI tool."""
 
     command = "arduino-lint"
-    lookbehind = "arduino-lint "
+    fail_on_warn = False
 
     def __init__(self, args: List[str]):
+        parser = ArgumentParser()
+        parser.add_argument("--fail-on-warn", action="store_true")
+        hook_args, cli_args = parser.parse_known_args(args)
+        self.fail_on_warn = hook_args.fail_on_warn
+        # arduino lint uses the project dir rather than filenames.
+        cli_args.append(Path.cwd().resolve().__str__())
         super().__init__(
             self.command,
-            args,
+            cli_args,
             help_url="https://github.com/CreatingNull/Pre-Commit-Arduino"
         )
 
     def run(self):
         """Run arduino-lint."""
         self.run_command()
+        if self.fail_on_warn and " 0 WARNINGS" not in str(self.stdout):
+            print(str(self.stdout, encoding='UTF-8'))
+            self.raise_error(
+                "Failed due to arduino-lint warnings.",
+                "If this behaviour was not expected remove the '--fail-on-warn' flag."
+            )
 
 
 def main(argv=None):
