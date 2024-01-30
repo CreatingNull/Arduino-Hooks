f2f611fa51459471cfa8a316d5ffd7b4f5b9b907
MDW <mdeweerd@users.noreply.github.com>
2024-01-30 07:36:20 +0100

    Fix: Spelling (#33)
    Fixing Spelling
    Co-authored-by: MDW <--unset>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,13 @@ from arduino_hooks.arduino_lint import ArduinoLint
 
 @pytest.fixture(scope="function")
 def arduino_lint() -> ArduinoLint:
-    """Create an arduino lint object fixture for testing withs."""
+    """Create an arduino lint object fixture for testing with."""
     return ArduinoLint(["ArduinoLint"])  # Parameter mimics pre-commit argv[0]
 
 
 @pytest.fixture(scope="function")
 def arduino_cli() -> ArduinoCLI:
-    """Create an arduino cli object fixture for testing withs."""
+    """Create an arduino cli object fixture for testing with."""
     return ArduinoCLI(["ArduinoCLI", "--fqbn=arduino:avr:nano"])
 
 
